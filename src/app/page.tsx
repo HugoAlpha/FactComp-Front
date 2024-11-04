3759ebd4776317598cc9e27dd832457584dadd0c
Diego-pasante3 <pasante.software3@alphasys.com.bo>
2024-11-04 03:24:37 -0400

    [UPD] Updating Login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,20 @@ const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
+
+    if (!username || !password) {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Campos vacíos',
+        text: 'Por favor, complete ambos campos para iniciar sesión.',
+        showConfirmButton: false,
+        timer: 2000
+      });
+      return;
+    }
+
     try {
       const response = await fetch(`${PATH_URL_SECURITY}/api/usuarios/login`, {
         method: 'POST',
@@ -27,16 +39,16 @@ const Login = () => {
           password
         }).toString()
       });
-  
+
+      const data = await response.json();
+
       if (response.ok) {
-        const data = await response.json();
         if (data.response === "200") {
           localStorage.setItem("username", username);
-          
           localStorage.setItem("role", data.role);
           localStorage.setItem("tokenJWT", data.tokenJWT);
           localStorage.setItem("idEmpresa", data.id_empresa);
-  
+
           Swal.fire({
             position: "center",
             icon: 'success',
@@ -54,56 +66,68 @@ const Login = () => {
         } else {
           Swal.fire({
             icon: 'error',
-            title: 'Usuario no encontrado',
-            text: 'Por favor, verifica tus credenciales',
+            title: 'Error de autenticación',
+            text: data.response || 'Usuario o contraseña incorrectos.',
             showConfirmButton: false,
-            timer: 1500
+            timer: 2000
           });
         }
       } else {
         Swal.fire({
           icon: 'error',
           title: 'Error de servidor',
-          text: 'No se pudo conectar con el servidor',
+          text: data.response || 'No se pudo conectar con el servidor.',
           showConfirmButton: false,
-          timer: 1500
+          timer: 2000
         });
       }
     } catch (error) {
       console.error("Error de conexión:", error);
       Swal.fire({
         icon: 'error',
-        title: 'Error de conexión',
-        text: 'Hubo un problema al conectar con el servidor',
+        title: 'Error de inicio de sesion',
+        text: 'Hubo un problema, verifica usuario y contraseña',
         showConfirmButton: false,
-        timer: 1500
+        timer: 2000
       });
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-[#E6EFF7] to-[#F0F0F0]">
-      <div className="bg-white shadow-2xl rounded-lg flex w-5/6 h-5/6 max-w-6xl overflow-hidden">
-        <div className="md:w-1/2 p-6 md:p-10 flex flex-col justify-center bg-white">
-          <div className="mx-auto mb-4">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-[#d8e3e8] to-[#e8f3f5] animate-fade-in">
+      <div className="bg-white shadow-lg rounded-3xl flex flex-col md:flex-row w-full max-w-4xl overflow-hidden">
+        <div className="hidden md:flex md:w-1/2 bg-[#10314b] flex-col items-center justify-center p-8 text-white rounded-l-3xl shadow-lg relative">
+          <div className="flex flex-col items-center justify-center text-center animate-pulse">
+            <Image
+              src="/images/efactu2.png"
+              alt="Logo Alpha"
+              width={220}
+              height={220}
+              className="animate-scale-pulse"
+            />
+            <h2 className="text-3xl font-bold mt-6">Alpha E-Facturación</h2>
+            <p className="mt-4 text-lg px-4">Gestión eficiente de tus facturas con Alpha E-Facturación. ¡Hazlo fácil y rápido!</p>
+          </div>
+        </div>
+
+        <div className="md:w-1/2 p-10 flex flex-col justify-center bg-white rounded-r-3xl">
+          <div className="mx-auto mb-6">
             <Image
               src="/images/LogoIdAlpha.png"
               alt="Logo ID"
-              width={300}
-              height={300}
+              width={150}
+              height={150}
               priority
               className="max-w-full h-auto"
             />
           </div>
 
-          <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-6 text-center">Inicio de Sesión</h2>
+          <h2 className="text-3xl font-bold text-center text-[#10314b] mb-6">Inicio de Sesión</h2>
 
-          <form onSubmit={handleLogin} className="space-y-4 md:space-y-6">
+          <form onSubmit={handleLogin} className="space-y-4">
             <div>
-              <label htmlFor="username" className="block text-gray-700 mb-2">
-                Usuario
-              </label>
-              <div className="flex items-center border border-gray-300 rounded-lg px-4 py-3 bg-[#F9F9F9]">
+              <label htmlFor="username" className="block text-gray-600 mb-2">Usuario</label>
+              <div className="flex items-center border border-gray-300 rounded-lg px-4 py-2 bg-[#F1F1F1]">
                 <FaUserAlt className="text-gray-400 mr-3" />
                 <input
                   type="text"
@@ -111,16 +135,14 @@ const Login = () => {
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   placeholder="Ingresa tu usuario"
-                  className="bg-transparent outline-none w-full text-sm md:text-lg text-black"
+                  className="bg-transparent outline-none w-full text-gray-800"
                 />
               </div>
             </div>
 
             <div>
-              <label htmlFor="password" className="block text-gray-700 mb-2">
-                Contraseña
-              </label>
-              <div className="flex items-center border border-gray-300 rounded-lg px-4 py-3 bg-[#F9F9F9] relative">
+              <label htmlFor="password" className="block text-gray-600 mb-2">Contraseña</label>
+              <div className="flex items-center border border-gray-300 rounded-lg px-4 py-2 bg-[#F1F1F1] relative">
                 <FaLock className="text-gray-400 mr-3" />
                 <input
                   type={showPassword ? "text" : "password"}
@@ -128,7 +150,7 @@ const Login = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   placeholder="Ingresa tu Contraseña"
-                  className="bg-transparent outline-none w-full text-sm md:text-lg text-gray-800"
+                  className="bg-transparent outline-none w-full text-gray-800"
                 />
                 <button
                   type="button"
@@ -140,36 +162,21 @@ const Login = () => {
               </div>
             </div>
 
-            <div className="flex flex-col md:flex-row justify-between items-center mb-6">
-              <label className="flex items-center text-sm md:text-lg text-gray-600 mb-2 md:mb-0">
+            <div className="flex items-center justify-between mb-4">
+              <label className="flex items-center text-gray-600">
                 <input type="checkbox" className="mr-2" />
                 Recuérdame
               </label>
-              <a href="#" className="text-sm md:text-lg text-[#10314b] hover:text-gray-500">
-                ¿Olvidaste tu Password?
-              </a>
+              <a href="#" className="text-sm text-[#5086A8] hover:underline">¿Olvidaste tu Password?</a>
             </div>
 
             <button
               type="submit"
-              className="w-full bg-[#10314b] text-white font-semibold py-3 rounded-lg text-sm md:text-lg hover:bg-[#10314b] transition"
+              className="w-full bg-[#5086A8] text-white font-semibold py-2 rounded-lg hover:bg-[#10314b] transition-all duration-300 ease-in-out shadow-md"
             >
               Ingresar
             </button>
           </form>
-        </div>
-
-        {/* Logo y Recuadro */}
-        <div className="hidden md:flex md:w-1/2 bg-[#10314b] flex-col items-center justify-center p-12">
-          <div className="bg-white p-8 shadow-lg rounded-lg flex flex-col items-center">
-            <Image
-              src="/images/efactu2.png"
-              alt="Logo"
-              width={260}
-              height={260}
-            />
-            <h2 className="text-2xl md:text-3xl font-bold text-[#10314b] mt-4 md:mt-6">Alpha E-Facturación</h2>
-          </div>
         </div>
       </div>
     </div>
