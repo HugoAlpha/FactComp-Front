6ae314bfc813972efb5ac69ab9015598e99ece9a
Diego-pasante3 <pasante.software3@alphasys.com.bo>
2024-11-20 12:22:26 -0400

    [ADD] Adding new branch

--- src/app/manualBill/page.tsx
+++ src/app/manualBill/page.tsx
@@ -115,7 +115,7 @@ const ManualBill = () => {
             },
         });
     
-        fetch(`${PATH_URL_BACKEND}/factura/emitir-computarizada`, {
+        fetch(`${PATH_URL_BACKEND}/factura/emitir`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(factura),
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,3 @@
-export const PATH_URL_BACKEND = "http://10.1.70.202:8087/api/v1";
+export const PATH_URL_BACKEND = "http://10.1.70.202:8086/api/v1";
 export const PATH_URL_SECURITY = "http://10.1.70.202:8090";
 export const PATH_URL_IMAGES = "http://10.1.70.202:3086"
