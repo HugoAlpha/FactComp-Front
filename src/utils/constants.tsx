35cef20efed07dce3156cf9124fed9cf25025389
dyanez68 <maximilianoyaneztoro@gmail.com>
2024-12-10 12:28:25 -0400

    [UPD] Updating constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,3 @@
-export const PATH_URL_BACKEND = "http://10.1.5.193:5052/api/v1";
-export const PATH_URL_SECURITY = "http://10.1.5.193:5050";
-export const PATH_URL_IMAGES = "http://10.1.5.193:5081";
+export const PATH_URL_BACKEND = "http://190.181.63.219:5052/api/v1";
+export const PATH_URL_SECURITY = "http://190.181.63.219:5051/auth";
+export const PATH_URL_IMAGES = "http://190.181.63.219:5051/img";
