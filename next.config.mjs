9744dceaa2bdfa995933b8955440730ba8d648b6
hsaavedraAlpha <hugo.saavedrar@alphasys.com.bo>
2024-12-10 12:44:47 -0400

    [UPD] update the nextconfig for image service

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    images: {
-      domains: ['10.1.5.193'],
-    },
-  };
-  
-  export default nextConfig;
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['190.181.63.219'],
+  },
+};
+
+export default nextConfig;
