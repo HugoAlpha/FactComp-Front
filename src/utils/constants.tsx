7dc3f36a733ead58669a3f1bccd144a57ad85f16
hsaavedraAlpha <hugo.saavedrar@alphasys.com.bo>
2024-12-12 16:07:07 -0400

    [FIX] fixed the constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,3 @@
-export const PATH_URL_BACKEND = "http://190.181.63.219:5052/api/v1";
-export const PATH_URL_SECURITY = "http://190.181.63.219:5051/auth";
-export const PATH_URL_IMAGES = "http://190.181.63.219:5051/img";
+export const PATH_URL_BACKEND = "http://190.181.63.219:5054/api/v1";
+export const PATH_URL_SECURITY = "http://190.181.63.219:5053/auth";
+export const PATH_URL_IMAGES = "http://190.181.63.219:5053/img"
